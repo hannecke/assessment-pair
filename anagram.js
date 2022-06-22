4a3e6f30195e0cd4616f9eed4b1f2a4abb6c3510
marcos.hannecke <marcos@fundment.com>
2022-06-22 14:45:20 +0100

    first commit

---
+++ anagram.js
@@ -0,0 +1,25 @@
+/************
+ * ANAGRAM
+ ***********/
+
+// 1. Given two strings, return true if they are anagrams of one another
+
+/**
+ * Notes
+ * An anagram is any string that can be formed by shuffling the characters of the original string.
+ *  - Example: Mary is an anagram of Army.
+ * An empty string can be considered an anagram of itself.
+ */
+
+function isAnagram() {
+}
+
+// 2. Create a function that returns the smallest number of letter removals so that two strings
+//    - Example: minRemovals("abcde", "cab") ➞ 2
+//      Remove "d", "e" to make "abc" and "cab".
+//
+//    Notes: Characters won't be used more than once per string.
+
+function minRemovals() {
+
+}
